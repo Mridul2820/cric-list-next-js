f17134a0044bb7cafce4e79a00e2cdc849ee2e42
Mridul2820 <immridulpanda2820@gmail.com>
2021-03-31 20:34:44 +0530

    Adding Other Components

---
+++ components/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <h1>People List</h1>
+            </div>
+            <a>Home</a>
+            <a>About</a>
+            <a>People</a>
+        </nav>
+    );
+}
+ 
+export default NavBar;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 export default function Home() {
     return (
         <div>
+            <NavBar />
             <h1>Homepage</h1>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam expedita doloremque libero ratione quaerat non mollitia labore vel cumque culpa fugiat eligendi officia obcaecati qui autem quidem, ipsam, explicabo illum!</p>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam expedita doloremque libero ratione quaerat non mollitia labore vel cumque culpa fugiat eligendi officia obcaecati qui autem quidem, ipsam, explicabo illum!</p>
+            <Footer />
         </div>
     )
 }

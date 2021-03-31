f1b0a3f49f915a2db9974ffeec5614d51aede86c
Mridul2820 <immridulpanda2820@gmail.com>
2021-03-31 19:55:31 +0530

    pages & routes

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,11 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
-    return (
+const About = () => {
+    return ( 
         <div>
-            <h1>Homepage</h1>
+            <h1>About</h1>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam expedita doloremque libero ratione quaerat non mollitia labore vel cumque culpa fugiat eligendi officia obcaecati qui autem quidem, ipsam, explicabo illum!</p>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam expedita doloremque libero ratione quaerat non mollitia labore vel cumque culpa fugiat eligendi officia obcaecati qui autem quidem, ipsam, explicabo illum!</p>
         </div>
-    )
+    );
 }
+ 
+export default About;

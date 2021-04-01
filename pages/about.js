f3d7928d5e7987a5dc3f3c39e60262c1a9e2e2f0
Mridul2820 <immridulpanda2820@gmail.com>
2021-04-01 19:16:53 +0530

    adding metadata

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,18 @@
+import Head from "next/head";
+
 const About = () => {
     return ( 
-        <div>
-            <h1>About</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam expedita doloremque libero ratione quaerat non mollitia labore vel cumque culpa fugiat eligendi officia obcaecati qui autem quidem, ipsam, explicabo illum!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam expedita doloremque libero ratione quaerat non mollitia labore vel cumque culpa fugiat eligendi officia obcaecati qui autem quidem, ipsam, explicabo illum!</p>
-        </div>
+        <>
+            <Head>
+                <title>Cric List | About</title>
+                <meta name="keywords" content="About Cricket" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam expedita doloremque libero ratione quaerat non mollitia labore vel cumque culpa fugiat eligendi officia obcaecati qui autem quidem, ipsam, explicabo illum!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam expedita doloremque libero ratione quaerat non mollitia labore vel cumque culpa fugiat eligendi officia obcaecati qui autem quidem, ipsam, explicabo illum!</p>
+            </div>
+        </>
     );
 }
  
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
+import Head from 'next/head'
+
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
 export default function Home() {
     return (
-        <div>
-            <h1 className={styles.title}>Homepage</h1>
-            <p className={styles.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam expedita doloremque libero ratione quaerat non mollitia labore vel cumque culpa fugiat eligendi officia obcaecati qui autem quidem, ipsam, explicabo illum!</p>
-            <p className={styles.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam expedita doloremque libero ratione quaerat non mollitia labore vel cumque culpa fugiat eligendi officia obcaecati qui autem quidem, ipsam, explicabo illum!</p>
+        <>
+            <Head>
+                <title>Cric List | Home</title>
+                <meta name="keywords" content="cricket" />
+            </Head>
+            <div>
+                <h1 className={styles.title}>Homepage</h1>
+                <p className={styles.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam expedita doloremque libero ratione quaerat non mollitia labore vel cumque culpa fugiat eligendi officia obcaecati qui autem quidem, ipsam, explicabo illum!</p>
+                <p className={styles.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam expedita doloremque libero ratione quaerat non mollitia labore vel cumque culpa fugiat eligendi officia obcaecati qui autem quidem, ipsam, explicabo illum!</p>
 
-            <Link href="/cric">
-                <a className={styles.btn}>See Cric List</a>
-            </Link>
-        </div>
+                <Link href="/cric">
+                    <a className={styles.btn}>See Cric List</a>
+                </Link>
+            </div>
+        </>
     )
 }

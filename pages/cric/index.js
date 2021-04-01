ec57207f44e8f2b3b1928908d5e9798f01cac950
Mridul2820 <immridulpanda2820@gmail.com>
2021-04-01 22:24:47 +0530

    calling the cric api

--- pages/cric/index.js
+++ pages/cric/index.js
@@ -1,8 +1,28 @@
+import styles from '../../styles/Cric.module.css'
 
-const cric = () => {
+export const getStaticProps = async() => {
+    const res = await fetch('https://cricapi.com/api/matches?apikey=MdhmWyZAEDMytcLVHzKhs9Rrx0G2')
+    const data = await res.json()
+
+    // console.log(data.matches);
+
+    return {
+        props: {crics: data.matches}
+    }
+}
+
+const cric = ({ crics }) => {
     return (
         <div>
             <h1>Cric List</h1>
+            {crics.map(cric => (
+                <div key={cric.unique_id}>
+                    <a className={styles.single}>
+                        <h3>{cric["team-1"]} vs {cric["team-2"]}</h3>
+                        <h3>Starting time {new Date(cric.dateTimeGMT).toLocaleString()}</h3>
+                    </a>
+                </div>
+            ))}
         </div>
     )
 }
